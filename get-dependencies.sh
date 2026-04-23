1c9244dc2748a1c8a4b4ebd2a61d063aa6cc7e53
Link4Electronics <link4electronics@gmail.com>
2026-04-23 07:21:28 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,11 +19,11 @@ get-debloated-pkgs --add-common --prefer-nano
 # If the application needs to be manually built that has to be done down here
 echo "Getting app..."
 echo "---------------------------------------------------------------"
-case "$ARCH" in # they use AMD64 and ARM64 for the deb links
-	x86_64)  deb_arch=amd64;;
+case "$ARCH" in # they use ARM64 for the deb links
+	x86_64)  deb_arch=x86_64;;
 	aarch64) deb_arch=arm64;;
 esac
-DEB_LINK=$(wget https://api.github.com/repos/shiftkey/desktop/releases/latest -O - \
+DEB_LINK=$(wget https://api.github.com/repos/pol-rivero/github-desktop-plus/releases/latest -O - \
    | sed 's/[()",{} ]/\n/g' | grep -o -m 1 "https.*$deb_arch.*.deb")
 echo "$DEB_LINK" | awk -F'/' '{val=$(NF-1); gsub(/^release-/, "", val); gsub(/-linux1$/, "", val); print val; exit}' > ~/version
 if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app.deb 2>/tmp/download.log; then
